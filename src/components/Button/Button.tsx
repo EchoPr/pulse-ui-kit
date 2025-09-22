1ffd77f8b3ad4ee92a4bf46c90bc266b60523841
m1ster-king <ivanmikov2006@gmail.com>
2025-09-22 13:28:09 +0300

    Added default button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import "./Button.scss";
 
-export const Button = () => {
-  return <button>Button</button>;
-};
+type style = "primary" | "secondary" | "danger" | "default";
+type size = "small" | "normal";
+
+interface ButtonProps {
+  text: string;
+  style?: style;
+  size?: size;
+}
+
+export default function Button({
+  text,
+  style = "primary",
+  size = "normal",
+}: ButtonProps) {
+  return <button className={`btn btn-${style} btn-${size}`}>{text}</button>;
+}
