89216366cfe7e7503c5acd83bfc9b23b1b6bfd39
m1ster-king <ivanmikov2006@gmail.com>
2025-09-25 22:53:28 +0300

    Create danger button variations

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,18 +1,32 @@
-import "./Button.scss";
+import React from 'react';
 
-type style = "primary" | "secondary" | "danger" | "default";
-type size = "small" | "normal";
+import './Button.module.scss';
+
+type variant = 'default' | 'danger';
+type style = 'primary' | 'secondary' | 'on-danger' | 'on-default';
+type size = 'small' | 'normal';
 
 interface ButtonProps {
   text: string;
+  variant?: variant;
   style?: style;
   size?: size;
+  onClick?: () => void;
 }
 
 export default function Button({
   text,
-  style = "primary",
-  size = "normal",
+  variant,
+  style = 'primary',
+  size = 'normal',
+  onClick,
 }: ButtonProps) {
-  return <button className={`btn btn-${style} btn-${size}`}>{text}</button>;
+  return (
+    <button
+      className={`btn btn-${variant}-${style} btn-${size}`}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
 }
